c37bca42ccd1b398c33d62b57954aac284c79f78
easonnong <nongeason@gmail.com>
2022-11-06 22:39:59 +0800

    config

--- TestUniswapV2/hardhat.config.js
+++ TestUniswapV2/hardhat.config.js
@@ -25,9 +25,9 @@ const REPORT_GAS = process.env.REPORT_GAS || false
 
 // VPN
 // yarn add undici
-const { setGlobalDispatcher, ProxyAgent } = require("undici")
-const proxyAgent = new ProxyAgent("http://127.0.0.1:19180")
-setGlobalDispatcher(proxyAgent)
+// const { setGlobalDispatcher, ProxyAgent } = require("undici")
+// const proxyAgent = new ProxyAgent("http://127.0.0.1:19180")
+// setGlobalDispatcher(proxyAgent)
 
 module.exports = {
   defaultNetwork: "hardhat",
@@ -37,6 +37,9 @@ module.exports = {
       forking: {
         url: MAINNET_RPC_URL,
         //blockNumber: FORKING_BLOCK_NUMBER,
+        httpHeaders: {
+          Authorization: `Bearer ${process.env.ETHERSCAN_API_KEY}`,
+        },
         enabled: true,
       },
       chainId: 31337,
--- TestUniswapV2/test/unit/UniswapV2_unit_test.js
+++ TestUniswapV2/test/unit/UniswapV2_unit_test.js
@@ -4,8 +4,9 @@ const { network, deployments, ethers } = require("hardhat")
 const { developmentChains } = require("../../helper-hardhat-config")
 
 const { DAI, WBTC, WBTC_WHALE } = require("../config")
-const ERC20ABI = require("../../node_modules/@openzeppelin/contracts/build/contracts/ERC20.json")
-const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC_URL)
+const DAI_ABI = require("../ABI/dai_abi.json")
+const WBTC_ABI = require("../ABI/wbtc_abi.json")
+//const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC_URL)
 
 !developmentChains.includes(network.name)
   ? describe.skip
@@ -15,15 +16,19 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC_UR
       async function deployContractLockFixture() {
         let testUniswapV2, testUniswapV2Contract
         const accounts = await ethers.getSigners()
+        const provider = ethers.provider
         const deployer = accounts[0]
         const user = accounts[1]
+        console.log(`1`)
         await deployments.fixture(["all"])
+        console.log(`1`)
         testUniswapV2Contract = await ethers.getContract("TestUniswapV2")
+        console.log(`1`)
         testUniswapV2 = testUniswapV2Contract.connect(deployer)
         console.log(`1`)
-        const DAIContract = new ethers.Contract(DAI, ERC20ABI, provider)
+        const DAIContract = new ethers.Contract(DAI, DAI_ABI, provider)
         console.log(`2`)
-        const WbtcContract = new ethers.Contract(WBTC, ERC20ABI, provider)
+        const WbtcContract = new ethers.Contract(WBTC, WBTC_ABI, provider)
         return { testUniswapV2, testUniswapV2Contract, DAIContract, WbtcContract, deployer, user }
       }
 
@@ -32,24 +37,29 @@ const provider = new ethers.providers.JsonRpcProvider(process.env.MAINNET_RPC_UR
           let { testUniswapV2, testUniswapV2Contract, DAIContract, WbtcContract, deployer } =
             await loadFixture(deployContractLockFixture)
 
+          await network.provider.request({
+            method: "hardhat_impersonateAccount",
+            params: [WBTC_WHALE],
+          })
+
           const whale_signer = ethers.provider.getSigner(WBTC_WHALE)
           testUniswapV2 = testUniswapV2Contract.connect(whale_signer)
 
-          let DeployerDaiBalanceBefore = await DAIContract.balanceOf(deployer)
-          let DeployerWbtcBalanceBefore = await WbtcContract.balanceOf(deployer)
+          let DeployerDaiBalanceBefore = await DAIContract.balanceOf(deployer.address)
+          let DeployerWbtcBalanceBefore = await WbtcContract.balanceOf(deployer.address)
           console.log(`deployer dai:${DeployerDaiBalanceBefore}`)
           console.log(`deployer wbtc:${DeployerWbtcBalanceBefore}`)
-          let WhaleDaiBalanceBefore = await DAIContract.balanceOf(deployer)
-          let WhaleWbtcBalanceBefore = await WbtcContract.balanceOf(deployer)
+          let WhaleDaiBalanceBefore = await DAIContract.balanceOf(WBTC_WHALE)
+          let WhaleWbtcBalanceBefore = await WbtcContract.balanceOf(WBTC_WHALE)
           console.log(`whale dai:${WhaleDaiBalanceBefore}`)
           console.log(`whale wbtc:${WhaleWbtcBalanceBefore}`)
           await testUniswapV2.swap(WBTC, DAI, Wbtc_Input, 1, deployer.address)
-          let DeployerDaiBalanceAfter = await DAIContract.balanceOf(deployer)
-          let DeployerWbtcBalanceAfter = await WbtcContract.balanceOf(deployer)
+          let DeployerDaiBalanceAfter = await DAIContract.balanceOf(deployer.address)
+          let DeployerWbtcBalanceAfter = await WbtcContract.balanceOf(deployer.address)
           console.log(`deployer dai:${DeployerDaiBalanceAfter}`)
           console.log(`deployer wbtc:${DeployerWbtcBalanceAfter}`)
-          let WhaleDaiBalanceAfter = await DAIContract.balanceOf(deployer)
-          let WhaleWbtcBalanceAfter = await WbtcContract.balanceOf(deployer)
+          let WhaleDaiBalanceAfter = await DAIContract.balanceOf(WBTC_WHALE)
+          let WhaleWbtcBalanceAfter = await WbtcContract.balanceOf(WBTC_WHALE)
           console.log(`whale dai:${WhaleDaiBalanceAfter}`)
           console.log(`whale wbtc:${WhaleWbtcBalanceAfter}`)
         })
