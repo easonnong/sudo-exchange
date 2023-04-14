672852e73717761619323ed0572e6c1e4c85ea48
easonnong <nongeason@gmail.com>
2023-04-14 21:06:00 +0800

    feature: math

--- src/lib/Math.sol
+++ src/lib/Math.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: MIT
 pragma solidity 0.8.13;
 
-library Math {
+contract Math {
     function min(uint256 a, uint256 b) internal pure returns (uint256) {
         return a < b ? a : b;
     }
